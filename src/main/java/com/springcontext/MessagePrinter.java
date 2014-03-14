0e404bcaa7fb239e2d2f6a80e6339e7f6bec41ea
Swapnil Kamthe <swapnilkamthe@gmail.com>
2014-03-15 00:44:16 +0530

    Code correction & completed application

--- src/main/java/com/springcontext/MessagePrinter.java
+++ src/main/java/com/springcontext/MessagePrinter.java
@@ -1,12 +1,13 @@
 package com.springcontext;
 
-import org.springframework.beans.facory.annotation.Autowird;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
  
 @Component
 public class MessagePrinter 
 {
-	@Autowird
+	@Autowired
     private MessageService service;
 	
 	public void printMessage()
